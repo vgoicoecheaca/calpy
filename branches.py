d81931ab2b1c78a6ccd5e87c7db5325668c7d73b
VictorGoico <victorgc@hawaii.edu>
2022-06-15 17:56:58 +0200

    Migration to root, scratch

--- branches.py
+++ branches.py
@@ -1,46 +1,20 @@
-from multiprocessing import managers
-import sys
-import awkward as ak
-import uproot as ur
+import ROOT as R
 import numpy as np
 
 class Branches():
-    def __new__(cls, *args, **kwargs):
-        return super().__new__(cls)
-
     def __init__(self,manager):
-        self.m              = manager
-        self.branches_mc    = manager.config("branches","mc","str").split(",") 
-        self.branches       = {}
-        self.nevents        = manager.config("run","nevents","int")
+        self.manager        = manager
+        self.cut_stats      = {}
+
+    def __call__(self,file,tree):                   
+        self.file = R.TFile(file,"read")
+        self.tree = self.file.Get(tree)
+    
+        return self.tree, self.cut_stats
         
-    def __repr__(self) -> str:
-        return self.branches.keys()
+    def add_branch(self,name,func):       
+        self.tree.SetAlias(name,func)
 
-    def __call__(self,file,mode):    
-        self.mode = mode   
-        if mode =="mc":
-            branches_names = self.branches_mc
-        for branch in branches_names:
-            print("Reading file, branch",file,branch)
-            self.branches[branch] = self.read_branch(file,branch) 
-
-        self.change_branch(self.branches_mc,[branch+"_mc" for branch in self.branches_mc])
-
-        return self.branches
-
-    def add_branch(self,branch,data):
-        self.branches[branch] = data
-
-    def read_branch(self,fname,branch):       
-        return ur.open(fname)["dstree"][branch].array(library="np")[:self.nevents]
-
-    def cut_branches(self,cut,data="run"):
-        for key in self.branches.keys():
-            if data in key:
-                self.branches[key] = self.branches[key][cut]
-
-    def change_branch(self,olds, news):
-        for i in range(len(olds)):
-            self.branches[news[i]] = self.branches[olds[i]]
-            del self.branches[olds[i]]
+    def cut_branches(self,cut):           
+        self.tree = self.tree(cut)
+        self.cut_stats[cut] = self.tree.GetEntreis(cut)
--- run.py
+++ run.py
@@ -7,8 +7,44 @@ m = Manager()
 
 class Run():
     def __init__(self,fmc,fbg=None):  
-        self.branches = m.branches(m.config('run',"path",'str')+fmc,"mc") 
-        
+        self.tree_mc,self.cut_stats_mc  = m.branches(m.config('run',"path",'str')+fmc,"dstree") 
+        #self.tree_bg,self.cut_stats_bg  = None if fbg==None else m.branches(m.config('run',"path",'str')+fbg,"dstree") 
+        self.neutron_yield = 100;
+
+        # Apply cuts and store stats in dic for computing rates
+        print(self.cut_stats_mc) 
+        m.branches.cut_branches("depTPCtot>0")        
+        print(self.cut_stats_mc)
+ 
+    # Apply cuts and store stats in dic for computing rates
+    #m.branches.cut_branches(self.branches["depTPCtot"]>0)
+    #m.branches.
+
+    # events with energy deposit 
+    
+
+    #m.branches.cut_branches((self.branches["s1npe_run"]>s1min) & (self.branches["s1npe_run"]<s1max))
+
+    #nevts_edep                       = t1 ->GetEntries(cut_edep);
+    #nevets_singles 			         = t1 ->GetEntries("depTPCtot>0 && nclus==1");
+    #nevents_singles_NR               = t1->GetEntries("depTPCtot>0 && nclus_nucl==1 && nclus_elec==0"); 
+    #nevents_nr_tpc_w_gamma           = t1->GetEntries("depTPCtot>0 && nclus_nucl>0 && v2nclus_elec>0 && depVeto2tot>4200 && depVeto2tot<4500"); 
+    #nevents_nr_tpc_w_gamma_time      = t1->GetEntries("depTPCtot>0 && nclus_nucl>0 && v2nclus_elec>0 && v2cl_t[0]<2"); 
+    #source_livetime                  = nevts_tot/neutron_yield;
+
+
+
+    # implement an option for neutron vs gamma source and what's default by each of this options
+
+
+    # def a proccess to check for the PSD 
+
+
+    # def a process to check for XY reco
+
+
+
+
 
 ##### Run ####
 fmc  = m.config('run','mc','str')
