c694b5494105fbdfcad7f3cb9375085af94b9fba
vgoicoecheaca <vgoicoecheaca@gmail.com>
2022-09-19 07:10:37 -1000

    first commit

--- manager.py
+++ manager.py
@@ -1,9 +1,11 @@
 from plotter import Plotter 
 from config import Config
 from branches import Branches
+from aux import Aux
 
 class Manager():
     def __init__(self,**kwargs):
         self.config   = Config()
         self.plotter  = Plotter(manager=self,**kwargs)
         self.branches = Branches(manager=self,**kwargs)
+        self.aux      = Aux(manager=self,**kwargs)
