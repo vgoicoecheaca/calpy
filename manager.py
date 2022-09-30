63056e482e320d66671fde635d4c843fd8468250
vgoicoecheaca <vgoicoecheaca@gmail.com>
2022-09-29 16:43:57 -1000

    Migrating to cuts in separate file, added description

--- manager.py
+++ manager.py
@@ -5,7 +5,8 @@ from aux import Aux
 
 class Manager():
     def __init__(self,**kwargs):
-        self.config   = Config()
+        self.config   = Config("config.ini")
+        self.cuts     = Config("cuts.ini")
         self.plotter  = Plotter(manager=self,**kwargs)
         self.branches = Branches(manager=self,**kwargs)
         self.aux      = Aux(manager=self,**kwargs)
